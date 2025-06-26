87bea83701adb0e8462d3589299bd085756910aa
diana <diana.rubaga@alkemy.com>
2025-06-26 11:09:02 +0200

    refactor: change uppercase variables to lower to indicate functionality, in config and llm

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-# Model settings (safe defaults)
-OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o") 
-OPENAI_TEMPERATURE = float(os.getenv("OPENAI_TEMPERATURE", 0.1))
-OPENAI_MAX_TOKENS = int(os.getenv("OPENAI_MAX_TOKENS", 1500))
+# Model settings (safe defaults) lower case for not confidential and hardcoded values 
+openai_model = "gpt-4o"
+openai_temperature  = 0.1 
+openai_max_tokens = 1500
--- src/llm.py
+++ src/llm.py
@@ -4,13 +4,14 @@ from openai.types.chat.chat_completion import ChatCompletionMessage
 from tenacity import retry, wait_random_exponential, stop_after_attempt, retry_if_exception_type
 from pydantic import BaseModel
 from typing import List
-from src.config import OPENAI_API_KEY, OPENAI_MODEL, OPENAI_TEMPERATURE, OPENAI_MAX_TOKENS
+from src.config import OPENAI_API_KEY, openai_model, openai_temperature, openai_max_tokens
 
 
 class LLMRequest(BaseModel):
     system_prompt: str
     user_prompt: str
-
+# not in a class 
+# do readme 
 
 class LLMClient:
     def __init__(self):
@@ -21,18 +22,20 @@ class LLMClient:
       #  wait=wait_random_exponential(min=1, max=10),
        ## stop=stop_after_attempt(3),
     #)
+
+    #doing two things split it up get and forward messages (reforumulate function)
     def generate_response(self, request: LLMRequest) -> ChatCompletion:
         """
         Sends a prompt to OpenAI and returns the full response object.
         """
         return self.client.chat.completions.create(
-            model=OPENAI_MODEL,
+            model=openai_model,
             messages=[
                 {"role": "system", "content": request.system_prompt},
                 {"role": "user", "content": request.user_prompt}
             ],
-            temperature=OPENAI_TEMPERATURE,
-            max_tokens=OPENAI_MAX_TOKENS
+            temperature=openai_temperature,
+            max_tokens= openai_max_tokens 
         )
 
     def get_content(self, request: LLMRequest) -> str:
